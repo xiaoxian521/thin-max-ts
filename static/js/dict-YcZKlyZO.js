e589e782c196d94ff478ee6f971600df94328265
xiaoxian521 <1923740402@qq.com>
2025-12-07 19:58:19 +0800

    chore: 新增菜单风格功能

---
+++ static/js/dict-YcZKlyZO.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./dict.vue_vue_type_script_setup_true_lang-C249K6Pc.js";import"./rule-BIXlqqF3.js";import"./index-BuJlUaPD.js";export{o as default};
---
+++ static/js/form-gp1utWfL.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./form.vue_vue_type_script_setup_true_lang-Dakfp_Gf.js";import"./index-BuJlUaPD.js";import"./system-p1Vdiz9O.js";export{o as default};
